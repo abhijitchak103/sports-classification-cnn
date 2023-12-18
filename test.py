a8bd7d65576ab7b3a8d4dfe6a9726e9397a7fd67
abhijitchak103 <abhijityachak@gmail.com>
2023-12-18 20:35:09 +0530

    Correcting minor changes

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-
-data = {'url': 'https://images.pexels.com/photos/3628912/pexels-photo-3628912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
-
-result = requests.post(url, json=data).json()
-print(result)
